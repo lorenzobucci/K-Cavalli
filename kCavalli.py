5b499d18f8a5a351dde7460d19c030952e9785b3
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2018-12-18 22:33:54 +0100

    Implementazione iniziale della funzione per la risoluzione del problema [Rinominato il file py per incompatibilità]

---
+++ kCavalli.py
@@ -0,0 +1,36 @@
+import Numberjack as nj
+
+
+def disposizione_kCavalli(n: int, k: int):
+    m = nj.Matrix(n, n)  # Matrice di variabili booleane nxn
+    kCavalli = nj.Model()
+
+    # Vincoli di interdizione delle celle rispetto alla cella corrente
+    for i in range(n):
+        for j in range(n):
+            if j - 2 >= 0:
+                if i + 1 <= n - 1:
+                    kCavalli.add((m[i][j] + m[i + 1][j - 2]) != 2)
+                if i - 1 >= 0:
+                    kCavalli.add((m[i][j] + m[i - 1][j - 2]) != 2)
+            if j - 1 >= 0:
+                if i + 2 <= n - 1:
+                    kCavalli.add((m[i][j] + m[i + 2][j - 1]) != 2)
+                if i - 2 >= 0:
+                    kCavalli.add((m[i][j] + m[i - 2][j - 1]) != 2)
+            if j + 1 <= n - 1:
+                if i + 2 <= n - 1:
+                    kCavalli.add((m[i][j] + m[i + 2][j + 1]) != 2)
+                if i - 2 >= 0:
+                    kCavalli.add((m[i][j] + m[i - 2][j + 1]) != 2)
+            if j + 2 <= n - 1:
+                if i + 1 <= n - 1:
+                    kCavalli.add((m[i][j] + m[i + 1][j + 2]) != 2)
+                if i - 1 >= 0:
+                    kCavalli.add((m[i][j] + m[i - 1][j + 2]) != 2)
+
+    kCavalli.add(sum([m[i][j] for i in range(n) for j in range(n)]) == k)  # Vincolo per il numero di cavalli totali
+
+    solver: nj.NBJ_STD_Solver = kCavalli.load('Mistral')
+    solver.solve()
+    print(m)
