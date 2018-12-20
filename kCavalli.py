ab54bda89820562b7ea0b39dc634ef72c3ae4afe
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2018-12-20 19:25:06 +0100

    Ottimizzazione problema: eliminazione vincoli ridondanti

--- kCavalli.py
+++ kCavalli.py
@@ -8,26 +8,14 @@ def disposizione_kCavalli(n: int, k: int):
     # Vincoli di interdizione delle celle rispetto alla cella corrente
     for i in range(n):
         for j in range(n):
-            if j - 2 >= 0:
-                if i + 1 <= n - 1:
-                    kCavalli.add((m[i][j] + m[i + 1][j - 2]) != 2)
-                if i - 1 >= 0:
-                    kCavalli.add((m[i][j] + m[i - 1][j - 2]) != 2)
-            if j - 1 >= 0:
-                if i + 2 <= n - 1:
-                    kCavalli.add((m[i][j] + m[i + 2][j - 1]) != 2)
-                if i - 2 >= 0:
-                    kCavalli.add((m[i][j] + m[i - 2][j - 1]) != 2)
-            if j + 1 <= n - 1:
-                if i + 2 <= n - 1:
-                    kCavalli.add((m[i][j] + m[i + 2][j + 1]) != 2)
-                if i - 2 >= 0:
-                    kCavalli.add((m[i][j] + m[i - 2][j + 1]) != 2)
-            if j + 2 <= n - 1:
-                if i + 1 <= n - 1:
-                    kCavalli.add((m[i][j] + m[i + 1][j + 2]) != 2)
-                if i - 1 >= 0:
-                    kCavalli.add((m[i][j] + m[i - 1][j + 2]) != 2)
+            if j - 2 >= 0 and i + 1 <= n - 1:
+                kCavalli.add((m[i][j] + m[i + 1][j - 2]) != 2)
+            if j - 1 >= 0 and i + 2 <= n - 1:
+                kCavalli.add((m[i][j] + m[i + 2][j - 1]) != 2)
+            if j + 1 <= n - 1 and i + 2 <= n - 1:
+                kCavalli.add((m[i][j] + m[i + 2][j + 1]) != 2)
+            if j + 2 <= n - 1 and i + 1 <= n - 1:
+                kCavalli.add((m[i][j] + m[i + 1][j + 2]) != 2)
 
     kCavalli.add(sum([m[i][j] for i in range(n) for j in range(n)]) == k)  # Vincolo per il numero di cavalli totali
 
